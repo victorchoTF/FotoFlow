4bb250e8f70f271ed6ba91befd560041d92fc5bc
victorchotf <vrgeorgiev06@gmail.com>
2024-02-02 13:27:09 +0200

    init react app + post skeleton

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <Post />
+    </main>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/PostProfile.js
@@ -0,0 +1,14 @@
+import "./components.css";
+
+function PostProfile({username, image}){
+    return (
+        <div className="post-profile">
+            <img src={image} alt="profile"/>
+             <label className="username">
+                {username}
+             </label>
+        </div>
+    );
+}
+
+export default PostProfile;
